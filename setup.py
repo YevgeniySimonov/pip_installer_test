5f134df992198967c01b6ddf3f9ee0f4180f8a63
Yevgeniy Simonov <yevgeniy.simonov@theugroup.co>
2022-07-25 17:57:58 +0800

    test: updated setup.py

--- setup.py
+++ setup.py
@@ -1,37 +1,53 @@
-import sys
 import os
+import sys
 import subprocess
-from setuptools import setup
+
+from Cython.Build import cythonize
+from setuptools import find_packages
 from setuptools.command.build_ext import build_ext
-from setuptools.command.install import install
 
-# python3 setup.py build_ext --inplace
+try:
+    from setuptools import setup
+    from setuptools import Extension
+except ImportError:
+    from distutils.core import setup
+    from distutils.extension import Extension
 
-print(os.path.dirname(__name__))
+# compile: python3 setup.py build_ext --inplace
 
-# class BuildExt(build_ext):
-#     """Customized setuptools build_ext command - builds protos on build."""
-#     def run(self):
-#         protoc_command = ["make", "cython-build"]
-#         if subprocess.call(protoc_command) != 0:
-#             sys.exit(-1)
-#         build_ext.run(self)
+path = os.path.dirname(os.path.realpath(__file__))
+defs = [('NPY_NO_DEPRECATED_API', 0)]
+
+print('compillation path: ', path)
+
     
-class Install(install):
-    """Customized setuptools build_ext command - builds protos on build."""
-    def run(self):
-        protoc_command = ["make", "cython-build"]
-        if subprocess.call(protoc_command) != 0:
-            sys.exit(-1)
-        install.run(self)
-
 setup(
     author="Yevgeniy Simonov",
-    author_email="yevgeniy.simonov@theugroup.co",
-    description="Dummy Test",
-    packages=['pip_installer_test'],
-    include_dirs=['pip_installer_test/'],
-    has_ext_modules=lambda: True,
-    # cmdclass={'build_ext': BuildExt}
-    cmdclass={'install': Install}
-)
+    description="""Test pip installer""",
+    name="Test pip installer",
+    packages=find_packages(exclude=('tests', 'examples', 'docs')),
+    ext_modules=cythonize(
+        [
+            Extension(
+                "pip_installer_test.dummy_program",
+                sources=[
+                    "pip_installer_test/dummy_program.pyx",
+                    "pip_installer_test/src/dummyProgram.cpp"
+                ],
+                language='c++',
+                extra_compile_args=["-w", "-std=c++11"],
+                include_dirs=["pip_installer_test"],
+                define_macros=defs        
+            )
+        ], 
+        language_level = "3",
+        annotate=False,  # this flag if set to true generates html annotation
+        nthreads=4
+    ),
+    setup_requires=[
+        'setuptools >= 18.0',
+        'cython'
+    ],
+    zip_safe=True,
+    include_dirs=['pip_installer_test/']
+)   
