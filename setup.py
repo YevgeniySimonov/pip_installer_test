a5581611aabfdb5d2810e80c3e9975fda23a3b87
Yevgeniy Simonov <yevgeniy.simonov@theugroup.co>
2022-07-25 17:02:22 +0800

    test: checking makefile

--- setup.py
+++ setup.py
@@ -1,27 +1,43 @@
-import sys
 import os
-import subprocess
-from setuptools import setup
-from setuptools.command.build_ext import build_ext
+from Cython.Build import cythonize
 
-# python3 setup.py build_ext --inplace
+try:
+    from setuptools import setup
+    from setuptools import Extension
+except ImportError:
+    from distutils.core import setup
+    from distutils.extension import Extension
 
-print(os.path.dirname(__name__))
+# compile: python3 setup.py build_ext --inplace
 
-class BuildExt(build_ext):
-    """Customized setuptools build_ext command - builds protos on build."""
-    def run(self):
-        protoc_command = ["make", "cython-build"]
-        if subprocess.call(protoc_command) != 0:
-            sys.exit(-1)
-        build_ext.run(self)
-    
+path = os.path.dirname(os.path.realpath(__file__))
+defs = [('NPY_NO_DEPRECATED_API', 0)]
+
+print('compillation path: ', path)
+
+extending = Extension(
+    "dummy_program",
+    sources=[
+        os.path.normpath(os.path.join(path, 'pip_installer_test', 'dummy_program.pyx')),
+        os.path.normpath(os.path.join(path, 'pip_installer_test', 'src', 'dummyProgram.cpp'))
+    ],
+    language='c++',
+    extra_compile_args=["-w", "-std=c++11"],
+    include_dirs=["pip_installer_test"],
+    define_macros=defs        
+)
+
+extensions = [extending]
+
 setup(
     author="Yevgeniy Simonov",
-    author_email="yevgeniy.simonov@theugroup.co",
-    description="Dummy Test",
-    packages=['pip_installer_test'],
-    include_dirs=['pip_installer_test/'],
-    has_ext_modules=lambda: True,
-    cmdclass={'build_ext': BuildExt}
-)
+    description="""Test pip installer""",
+    name="Test pip installer",
+    ext_modules=cythonize(
+        extensions, 
+        language_level = "3",
+        annotate=False,  # this flag if set to true generates html annotation
+        nthreads=4
+    ),
+    zip_safe=True,
+)   
